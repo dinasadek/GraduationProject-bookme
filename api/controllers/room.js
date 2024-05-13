17852a7593c1d084e7874ee52c6a1dfa4ee3afc9
Lamiaa Atef <92232255+Lomy11@users.noreply.github.com>
2024-05-13 14:00:59 +0300

    Update room.js

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,6 +1,5 @@
-import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import Room from "../models/Room.js";
 
 export const createRoom = async (req, res, next) => {
   const hotelId = req.params.hotelid;
@@ -108,6 +107,42 @@ export const deleteOldDatesFromRooms = async (req, res, next) => {
     );
 
     res.status(200).json('Old dates have been deleted from room availability.');
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const deleteDatesFromRooms = async (req, res, next) => {
+  try {
+    const { roomDetails, dates } = req.body;
+
+    // Iterate over each room detail
+    for (const roomDetail of roomDetails) {
+      const { roomid, selectedRoomsId } = roomDetail;
+
+      // Find the room by its ID
+      const room = await Room.findById(roomid);
+      if (!room) {
+        throw new Error(`Room with ID ${roomid} not found.`);
+      }
+
+      // Iterate over selected room numbers
+      for (const roomNumberId of selectedRoomsId) {
+        const roomNumber = room.roomNumbers.find(num => num._id.toString() === roomNumberId);
+        if (!roomNumber) {
+          throw new Error(`Room number with ID ${roomNumberId} not found in room with ID ${roomid}.`);
+        }
+
+        // Remove specified dates from unavailableDates
+        roomNumber.unavailableDates = roomNumber.unavailableDates.filter(date => !dates.includes(date.toDateString()));
+      }
+
+      // Save the updated room
+      await room.save();
+    }
+
+    res.status(200).json('Selected dates have been deleted from room availability.');
   } catch (err) {
     next(err);
   }
